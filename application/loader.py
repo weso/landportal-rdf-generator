4fca829ec036298115e7b62e0327495052eb919b
Guillermo Infante Hernández <guillermo2104@gmail.com>
2014-04-22 14:58:33 +0200

    Fix genration path

--- application/loader.py
+++ application/loader.py
@@ -4,6 +4,6 @@ import sh
 
 def load_data_set(host, api, graph_uri):
     sh.curl(host+api+graph_uri,
-       digest=True, u="dba:root", verbose=True, X="POST", T="generated/../generated/dataset.ttl")
+       digest=True, u="dba:root", verbose=True, X="POST", T="../generated/dataset.ttl")
 
 
